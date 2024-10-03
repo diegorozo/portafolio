b8c4bb8469960fd81d96d7646f0c1fa5b1584a06
Diego A. Rozo M. <rozdiego@gmail.com>
2024-10-03 07:52:32 -0500

    potafolio nuevo

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-
-import robotsTxt from "astro-robots-txt"
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://www.rozdiego.com',
-})
